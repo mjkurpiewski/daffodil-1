f1642354b73db178c83669a36ab27ea193d92f4c
Peter Kurpiewski <pkurpiewski@gmail.com>
2015-08-24 12:33:32 -0400

    added lanterna dependency to project.clj

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject gameengine "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A fairly generic engine for implementing board games."
+  :url "http://orbistertius.xyz"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+		 [clojure-lanterna "0.9.4"]]]
   :main ^:skip-aot gameengine.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
