7c4ae948416fe033809978c59b09597d0f62b7ed
Peter Kurpiewski <pkurpiewski@gmail.com>
2015-08-24 16:03:11 -0400

    fixed project.clj syntax error.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-		 [clojure-lanterna "0.9.4"]]]
+		 [clojure-lanterna "0.9.4"]]
   :main ^:skip-aot gameengine.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
